deec776b9ce08e07ce8b1714ecfb143cfb8c2eb9
niv <n@e-ix.net>
2018-02-03 17:47:00 +0100

    8156: include .ltr and .nss, make use of new utility name

--- make-package.sh
+++ make-package.sh
@@ -17,7 +17,7 @@ cp -va "$NWN_ROOT"/bin/linux-x86/nwserver-linux data/bin/linux-x86/
 # cp -va "$NWN_ROOT"/bin/macos/nwserver-macos data/bin/macos/
 # cp -va "$NWN_ROOT"/bin/win32/nwserver.exe data/bin/win32/
 cp -va "$NWN_ROOT"/data/dialog.tlk data/data/
-nwn_resman_pkgsrv -d data/data --root "$NWN_ROOT"
+nwn_resman_pkg -d data/data --root "$NWN_ROOT"
 
 docker build -t beamdog/nwserver:$BRANCH -f Dockerfile .
 docker tag beamdog/nwserver:$BRANCH beamdog/nwserver:latest
